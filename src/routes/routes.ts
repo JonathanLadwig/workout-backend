43ad4804105ec07034bbee7237f45b7ad43c7932
Jonathan <jonathan.l1@contractor.curro.co.za>
2025-06-27 08:03:51 +0200

    feat: add MathController and routes for math question handling and chat functionality

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,9 @@
 import Elysia from "elysia";
 import { generatorRoutes } from "./generator.routes";
+import { mathRoutes } from "./math.routes";
 import { workoutRoutes } from "./workout.routes";
 
 export const routes = new Elysia()
     .use(generatorRoutes)
     .use(workoutRoutes)
+    .use(mathRoutes)
