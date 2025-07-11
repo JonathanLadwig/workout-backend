60956ed297b031eb80836f3aa3345e5160a5f8ef
Jonathan <jonathan.l1@contractor.curro.co.za>
2025-07-11 14:40:25 +0200

    feat: integrate generator and workout routes into Elysia application

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,7 @@
 import Elysia from "elysia";
 import { generatorRoutes } from "./generator.routes";
-import { mathRoutes } from "./math.routes";
 import { workoutRoutes } from "./workout.routes";
 
 export const routes = new Elysia()
     .use(generatorRoutes)
     .use(workoutRoutes)
-    .use(mathRoutes)
